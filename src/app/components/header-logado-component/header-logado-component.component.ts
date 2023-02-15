58f16bae1b0560060e538f0eff31b79e2ac93326
KAUÊ RAFAEL VOLLES FREITAS TEIXEIRA <kaue_teixeira@estudante.sesisenai.org.br>
2023-02-15 15:55:22 -0300

    header logado

---
+++ src/app/components/header-logado-component/header-logado-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header-logado-component',
+  templateUrl: './header-logado-component.component.html',
+  styleUrls: ['./header-logado-component.component.css']
+})
+export class HeaderLogadoComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
