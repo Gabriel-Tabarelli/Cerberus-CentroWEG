f04d3f4fbe59db6413a51af30790b3dc6d874424
KAUÊ RAFAEL VOLLES FREITAS TEIXEIRA <kaue_teixeira@estudante.sesisenai.org.br>
2023-07-07 16:52:09 -0300

    Doing the edit page and fixing signup component

--- src/app/components/cadastro-component/cadastro.component.ts
+++ src/app/components/cadastro-component/cadastro.component.ts
@@ -1,15 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-cadastro',
   templateUrl: './cadastro.component.html',
   styleUrls: ['./cadastro.component.css']
 })
-export class CadastroComponent {
+export class CadastroComponent implements OnInit {
 
+  ngOnInit(): void {
+    console.log(this.edicao)
+  
+    const User = {
+      nome: 'John Doe',
+      cpf: '12345678901',
+      estado: 'São Paulo',
+      cidade: 'São Paulo',
+      bairro: 'Centro',
+      endereco: 'Rua Principal',
+      numero: 123,
+      cep: '12345-678',
+      telefone: 987654321,
+      nomeContato: 'Jane Doe',
+      email: 'johndoe@example.com',
+      senha: 'password'
+    };
+
+    this.nome = User.nome;
+    this.cpf = User.cpf;
+    this.estado = User.estado;
+    this.cidade = User.cidade;
+    this.bairro = User.bairro;
+    this.endereco = User.endereco;
+    this.numero = User.numero;
+    this.cep = User.cep;
+    this.telefone = User.telefone;
+    this.nomeContato = User.nomeContato;
+    this.email = User.email;
+    this.senha = User.senha;
+
+    if (this.cpf == null) {
+      this.pessoaJuridica();	
+    } else {
+      this.pessoaFisica();
+    }
+
+    if (this.edicao) {
+      this.titulo = "Edição";
+      this.acao = "Editar";
+    }
+
+  }
+
+  @Input()
+  edicao: boolean = false;
+  acao: string = "Cadastrar";
+  titulo: string = "Cadastro";
   textNome:string = "Nome";
   textId:string = "CPF";
-  boolean:boolean = true;
+  
+  pessoa:boolean = true;
 
   // --Dados do Usuario-- //
   lista: user[] = [];
@@ -28,25 +77,20 @@ export class CadastroComponent {
   senha:string;
   //---
   submit() {
-    this.lista.push(new user(this.nome, this.cpf, this.estado, this.cidade, this.bairro, this.endereco, this.numero, this.cep, this.telefone, this.nomeContato, this.email, this.senha, !this.boolean))
+    this.lista.push(new user(this.nome, this.cpf, this.estado, this.cidade, this.bairro, 
+      this.endereco, this.numero, this.cep, this.telefone, this.nomeContato, this.email, this.senha))
     console.log(this.lista)
   }
   // --------end---------
 
-  troca1(){
-    if(this.boolean) {
+  pessoaJuridica(){
       this.textNome = "Empresa"
       this.textId = "CNPJ";
-      this.boolean = false;
-    }
   }
 
-  troca2(){
-    if(!this.boolean) {
+  pessoaFisica(){
       this.textNome = "Nome"
       this.textId = "CPF";
-      this.boolean = true;
-    }
   }
 
 }
@@ -66,7 +110,7 @@ class user{
   senha:string;
   empresa:boolean;
 
-  constructor(nome:string, cpf:string, estado:string, cidade:string, bairro:string, endereco:string, numero:number, cep:string, telefone:number, nomeContato:string, email:string, senha:string, empresa:boolean) {
+  constructor(nome:string, cpf:string, estado:string, cidade:string, bairro:string, endereco:string, numero:number, cep:string, telefone:number, nomeContato:string, email:string, senha:string) {
       this.nome = nome;
       this.cpf = cpf;
       this.estado = estado;
@@ -79,6 +123,5 @@ class user{
       this.nomeContato = nomeContato;
       this.email = email;
       this.senha = senha;
-      this.empresa = empresa;
   }
 }
---
+++ src/app/pages/ordered-page/ordered-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ordered-page',
+  templateUrl: './ordered-page.component.html',
+  styleUrls: ['./ordered-page.component.css']
+})
+export class OrderedPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
